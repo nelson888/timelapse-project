07c560bfa60e1375f03854004dc233da13b55f19
Fonkoua Tambue Nelson <nfonkoua@weborama.com>
2018-11-29 17:40:27 +0100

    started implementations of AbstractTimelapseClient

--- timelapse-api-client/src/main/java/com/app4/project/timelapse/api/client/TimelapseAsyncClient.java
+++ timelapse-api-client/src/main/java/com/app4/project/timelapse/api/client/TimelapseAsyncClient.java
@@ -1,173 +1,26 @@
 package com.app4.project.timelapse.api.client;
 
-import com.app4.project.timelapse.model.CameraState;
-import com.app4.project.timelapse.model.Command;
-import com.app4.project.timelapse.model.ErrorResponse;
-import com.app4.project.timelapse.model.Execution;
-import com.app4.project.timelapse.model.FileResponse;
-import com.app4.project.timelapse.model.GlobalState;
 import com.app4.project.timelapse.model.User;
-import com.google.gson.Gson;
+
 import com.tambapps.http.restclient.RestClient;
 import com.tambapps.http.restclient.request.RestRequest;
-
-import com.tambapps.http.restclient.request.handler.output.BodyHandlers;
 import com.tambapps.http.restclient.request.handler.response.ResponseHandler;
-import com.tambapps.http.restclient.request.handler.response.ResponseHandlers;
-import com.tambapps.http.restclient.response.RestResponse;
-import com.tambapps.http.restclient.util.ISSupplier;
 
-import java.io.File;
+public class TimelapseAsyncClient extends AbstractTimelapseClient {
 
-public class TimelapseClient implements TimelapseAPIClient {
 
-  private static final String API_ENDPOINT = "api/";
-  private static final String FILE_STORAGE_ENDPOINT = "files/";
-  private final RestClient client;
-  private final Gson gson = new Gson();
 
-  //TODO handle authentication with server and jwt
-  public TimelapseClient(String baseUrl, User user) {
-    client = new RestClient(baseUrl);
-    //TODO authenticate handle jwt
-    /*
-    RestResponse<String, String> response = client.execute(RestRequest.builder()
-            .POST()
-            .endpoint(API_ENDPOINT + "/authenticate")
-            .output(BodyHandlers.json(gson.toJson(user)))
-            .build(),
-        ResponseHandlers.stringHandler());
-    if (!response.isSuccessful() || response.isErrorResponse()) {
-      throw new RuntimeException("Couldn't authenticate");
-    }
-
-    String jwt = gson.fromJson(response.getSuccessData(), AuthResponse.class).getJwt();
-    client.setJwt(jwt);*/
+  public TimelapseAsyncClient(String baseUrl, User user) {
+    super(baseUrl, user);
   }
 
-  public void postCommand(Command command, Callback<Command> callback) {
-    postObject(API_ENDPOINT + "command", command, callback);
+  @Override
+  <T> void executeRequest(RestClient client, RestRequest request,
+      ResponseHandler<T> responseHandler, RestClient.Callback<T, T> callback) {
+    client.executeAsync(request);
   }
 
-  public void putCameraState(CameraState cameraState, Callback<CameraState> callback) {
-    putObject(API_ENDPOINT + "state", cameraState, callback);
-  }
+  @Override public void shutdown() {
 
-  public void postExecution(Execution execution, Callback<Execution> callback) {
-    postObject(API_ENDPOINT + "execution", execution, callback);
-  }
-
-  public void getCommand(Callback<Command> callback) {
-    getObject(API_ENDPOINT + "command", Command.class, callback);
-  }
-
-  public void getExecution(Callback<Execution> callback) {
-    getObject(API_ENDPOINT + "execution", Execution.class, callback);
-  }
-
-  public void getGlobalState(Callback<GlobalState> callback) {
-    getObject(API_ENDPOINT + "globalState", GlobalState.class, callback);
-  }
-
-  public void getCameraState(Callback<CameraState> callback) {
-    getObject(API_ENDPOINT + "state", CameraState.class, callback);
-  }
-
-  public void putImage(ISSupplier isSupplier, Callback<FileResponse> callback, int executionId) {
-    RestRequest request = RestRequest.builder(FILE_STORAGE_ENDPOINT + executionId)
-        .PUT()
-        .output(BodyHandlers.multipartStream(isSupplier, "image"))
-        .build();
-    client.executeAsync(request, ResponseHandlers.stringHandler(),
-        generateRestCallback(callback, FileResponse.class));
-  }
-
-  public void putImage(File file, Callback<FileResponse> callback, int executionId) {
-    RestRequest request = RestRequest.builder(FILE_STORAGE_ENDPOINT + executionId)
-        .PUT()
-        .output(BodyHandlers.multipartFile(file))
-        .build();
-    client.executeAsync(request, ResponseHandlers.stringHandler(),
-        generateRestCallback(callback, FileResponse.class));
-  }
-
-  //RestResponseHandler: InputStream -> Bitmap avec BitmapFactory.decodeStream(is)
-  public <T> void getImage(ResponseHandler<T> responseHandler, final Callback<T> callback, int executionId, int fileId) {
-    RestRequest request = RestRequest.builder(FILE_STORAGE_ENDPOINT + executionId + "/" + fileId)
-        .GET()
-        .build();
-    client.executeAsync(request, responseHandler, ResponseHandlers.stringHandler(),
-        new RestClient.Callback<T, String>() {
-          @Override
-          public void call(RestResponse<T, String> response) {
-            if (response.isSuccessful() && !response.isErrorResponse()) {
-              callback.onSuccess(response.getResponseCode(), response.getSuccessData());
-            } else {
-              callback.onError(response.getResponseCode(), new ErrorResponse(response.getErrorData()));
-            }
-          }
-    });
-  }
-
-  public Integer getImagesCount(int executionId, final Callback<Integer> callback) {
-    RestRequest request = RestRequest.builder(FILE_STORAGE_ENDPOINT + executionId + "/count")
-        .GET()
-        .build();
-    client.executeAsync(request, ResponseHandlers.intHandler(), ResponseHandlers.stringHandler(), new RestClient.Callback<Integer, String>() {
-      @Override
-      public void call(RestResponse<Integer, String> restResponse) {
-        if (restResponse.isSuccessful()) {
-          callback.onSuccess(restResponse.getResponseCode(), restResponse.getSuccessData());
-        } else {
-          callback.onError(restResponse.getResponseCode(), gson.fromJson(restResponse.getErrorData(), ErrorResponse.class));
-        }
-      }
-    });
-    RestResponse<Integer, ?> response = client.execute(request, ResponseHandlers.intHandler());
-    return response.isErrorResponse() ? null : response.getSuccessData();
-  }
-
-  private <T> void getObject(String endpoint, Class<T> clazz, Callback<T> callback) {
-    RestRequest request = RestRequest.builder(endpoint)
-        .GET()
-        .build();
-    client.executeAsync(request, ResponseHandlers.stringHandler(),
-        generateRestCallback(callback, clazz));
-  }
-
-  private <T> void putObject(String endpoint, T object, Callback<T> callback) {
-    requestObject(RestRequest.PUT, endpoint, object, callback);
-  }
-
-  private <T> void postObject(String endpoint, T object, Callback<T> callback) {
-    requestObject(RestRequest.POST, endpoint, object, callback);
-  }
-
-  private <T> void requestObject(String method, String endpoint, T object, Callback<T> callback) {
-    Class<T> clazz = (Class<T>) object.getClass();
-    RestRequest request = RestRequest.builder(endpoint).method(method)
-        .output(BodyHandlers.json(gson.toJson(object)))
-        .build();
-    client.executeAsync(request, ResponseHandlers.stringHandler(),
-        generateRestCallback(callback, clazz));
-  }
-
-  private <T> RestClient.Callback<String, String> generateRestCallback(final Callback<T> callback, final Class<T> clazz) {
-    return new RestClient.Callback<String, String>() {
-      @Override
-      public void call(RestResponse<String, String> response) {
-        if (response.isSuccessful()) {
-          callback.onSuccess(response.getResponseCode(), gson.fromJson(response.getSuccessData(), clazz));
-        } else if (response.isErrorResponse()) {
-          callback.onError(response.getResponseCode(), gson.fromJson(response.getErrorData(), ErrorResponse.class));
-        } else { //has exception
-          callback.onError(RestResponse.REQUEST_NOT_SENT, new ErrorResponse(response.getException().getMessage()));
-        }
-      }
-    };
-  }
-
-  public void shutdown() {
-    client.shutDown();
   }
 }
--- timelapse-api-client/src/main/java/com/app4/project/timelapse/api/client/TimelapseFakeClient.java
+++ timelapse-api-client/src/main/java/com/app4/project/timelapse/api/client/TimelapseFakeClient.java
@@ -1,4 +1,74 @@
 package com.app4.project.timelapse.api.client;
 
+import com.app4.project.timelapse.model.CameraState;
+import com.app4.project.timelapse.model.Command;
+import com.app4.project.timelapse.model.Execution;
+import com.app4.project.timelapse.model.GlobalState;
+import com.google.gson.Gson;
+import com.tambapps.http.restclient.RestClient;
+
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+
 public class TimelapseFakeClient {
+  private static final String API_ENDPOINT = "api/";
+  private static final String FILE_STORAGE_ENDPOINT = "files/";
+  private static final int RESPONSE_SUCCESS = 200;
+  private final Gson gson = new Gson();
+
+  private final Queue<Execution> executions = new PriorityQueue<>(10);
+  private final Queue<Command> commands = new ArrayDeque<>();
+  private CameraState cameraState = new CameraState();
+
+
+  public void putCommand(Command command, Callback<Command> callback) {
+    commands.add(command);
+    callback.onSuccess(RESPONSE_SUCCESS, command);
+  }
+
+  public void postCommand(Command command, Callback<Command> callback) {
+    putCommand(command, callback);
+  }
+
+  public void putCameraState(CameraState cameraState, Callback<CameraState> callback) {
+    this.cameraState = cameraState;
+    callback.onSuccess(RESPONSE_SUCCESS, cameraState);
+  }
+
+  public void putExecution(Execution execution, Callback<Execution> callback) {
+    executions.add(execution);
+    callback.onSuccess(RESPONSE_SUCCESS, execution);
+  }
+
+  public void postExecution(Execution execution, Callback<Execution> callback) {
+    putExecution(execution, callback);
+  }
+
+  public void getCommand(Callback<Command> callback) {
+    //TODO
+  }
+
+  public void getExecution(Callback<Execution> callback) {
+    //TODO
+  }
+
+  public void getGlobalState(Callback<GlobalState> callback) {
+
+    callback.onSuccess(RESPONSE_SUCCESS,
+        new GlobalState(cameraState, this.executions.toArray(new Execution[0]),
+        commands.toArray(new Command[0])));
+  }
+
+  public void getCameraState(Callback<CameraState> callback) {
+    callback.onSuccess(RESPONSE_SUCCESS, cameraState);
+  }
+
+
+
+  public void shutdown() {
+
+  }
 }
