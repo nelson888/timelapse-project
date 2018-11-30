1338bb88ad494804fc01ea5b8a245c1aa5f8ac22
Fonkoua Tambue Nelson <nfonkoua@weborama.com>
2018-11-30 11:13:39 +0100

    added title to errorResponse

--- timelapse-data/src/main/java/com/app4/project/timelapse/model/ErrorResponse.java
+++ timelapse-data/src/main/java/com/app4/project/timelapse/model/ErrorResponse.java
@@ -1,14 +1,19 @@
 package com.app4.project.timelapse.model;
 
 public class ErrorResponse {
+  private String title;
   private String message;
 
-  public ErrorResponse(String message) {
+  public ErrorResponse(String title, String message) {
     this.message = message;
+    this.title = title;
   }
 
   public String getMessage() {
     return message;
   }
 
+  public String getTitle() {
+    return title;
+  }
 }
