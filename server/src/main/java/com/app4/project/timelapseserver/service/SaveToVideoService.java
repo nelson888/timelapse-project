ae9883a0a39ce2fe540c3e65f12f759307ae6fe0
nelson888 <tambue@hotmail.fr>
2020-03-25 16:10:38 +0100

    moved saving video process into SaveToVideoTask

--- server/src/main/java/com/app4/project/timelapseserver/service/SaveToVideoService.java
+++ server/src/main/java/com/app4/project/timelapseserver/service/SaveToVideoService.java
@@ -3,23 +3,16 @@ package com.app4.project.timelapseserver.service;
 import com.app4.project.timelapse.model.Execution;
 import com.app4.project.timelapse.model.SavingProgress;
 import com.app4.project.timelapse.model.SavingState;
-import com.app4.project.timelapseserver.codec.JpgSequenceEncoder;
-import com.app4.project.timelapseserver.exception.SavingException;
 import com.app4.project.timelapseserver.service.storage.StorageService;
-import com.app4.project.timelapseserver.util.IOSupplier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.app4.project.timelapseserver.service.task.SaveToVideoTask;
+
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class SaveToVideoService {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(SaveToVideoService.class);
 
   private final ExecutorService executor;
   private final StorageService storageService;
@@ -32,14 +25,11 @@ public class SaveToVideoService {
     this.executionSavingStateMap = executionSavingStateMap;
   }
 
-  // TODO add toTimestamp?
   public SavingState startVideoSaving(Execution execution, int fps, long fromTimestamp,
                                       long toTimestamp) {
     if (getSavingProgress(execution.getId()).getState() != SavingState.ON_GOING) {
-      LOGGER.info("Starting saving video for execution {} with fps {}", execution.getId(), fps);
-      executionSavingStateMap.put(execution.getId(), SavingProgress.onGoing(0));
       long framesCount = storageService.executionFilesCount(execution.getId(), fromTimestamp, toTimestamp);
-      executor.submit(() -> save(execution.getId(), fps, fromTimestamp, framesCount, toTimestamp));
+      executor.submit(new SaveToVideoTask(storageService, executionSavingStateMap, execution.getId(), fps, fromTimestamp, toTimestamp, framesCount));
     }
     return SavingState.ON_GOING;
   }
@@ -48,35 +38,4 @@ public class SaveToVideoService {
     return executionSavingStateMap.getOrDefault(executionId, SavingProgress.notStarted());
   }
 
-  private void save(int executionId, int fps, long fromTimestamp, long toTimestamp, long framesCount) {
-    long startTime = System.currentTimeMillis();
-    AtomicLong framesProcessed = new AtomicLong(0L);
-    try (JpgSequenceEncoder encoder = storageService.newEncoderForExecution(executionId, fps)) {
-      storageService.executionFiles(executionId, fromTimestamp, toTimestamp)
-        .forEach(supplier ->
-          addFrame(encoder, supplier,() -> updateProgress(executionId, framesProcessed, framesCount)));
-      executionSavingStateMap.put(executionId, SavingProgress.finished());
-      LOGGER.info("Finished saving video for execution {} (it took {}s)", executionId,
-        (System.currentTimeMillis() - startTime) / 1000L);
-    } catch (IOException | SavingException e) {
-      LOGGER.error("Error while saving video for execution {} (fps {})", e, fps, e);
-      executionSavingStateMap.put(executionId, SavingProgress.error());
-    }
-  }
-
-  private void updateProgress(int executionId, AtomicLong framesProcessed, long framesCount) {
-    int percentage = (int) (100L * framesProcessed.incrementAndGet() / framesCount);
-    executionSavingStateMap.put(executionId, SavingProgress.onGoing(percentage));
-  }
-  private void addFrame(JpgSequenceEncoder encoder, IOSupplier<byte[]> bytesSupplier,
-                        Runnable progressUpdater) {
-    try {
-      LOGGER.debug("Encoding frame");
-      byte[] bytes = bytesSupplier.get();
-      encoder.addFrame(bytes);
-      progressUpdater.run();
-    } catch (IOException e) {
-      throw new SavingException("Error while adding frame", e);
-    }
-  }
 }
---
+++ server/src/main/java/com/app4/project/timelapseserver/service/task/SaveToVideoTask.java
@@ -0,0 +1,63 @@
+package com.app4.project.timelapseserver.service.task;
+
+import com.app4.project.timelapse.model.SavingProgress;
+import com.app4.project.timelapseserver.codec.JpgSequenceEncoder;
+import com.app4.project.timelapseserver.exception.SavingException;
+import com.app4.project.timelapseserver.service.storage.StorageService;
+import com.app4.project.timelapseserver.util.IOSupplier;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+@AllArgsConstructor
+public class SaveToVideoTask implements Runnable {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(SaveToVideoTask.class);
+
+  private final StorageService storageService;
+  private final Map<Integer, SavingProgress> executionSavingStateMap;
+  private final int executionId;
+  private final int fps;
+  private final long fromTimestamp;
+  private final long toTimestamp;
+  private final long framesCount;
+  private final AtomicLong framesProcessed = new AtomicLong(0L);
+
+  @Override
+  public void run() {
+    LOGGER.info("Starting saving video for execution {} with fps {}", executionId, fps);
+    executionSavingStateMap.put(executionId, SavingProgress.onGoing(0));
+    long startTime = System.currentTimeMillis();
+    try (JpgSequenceEncoder encoder = storageService.newEncoderForExecution(executionId, fps)) {
+      storageService.executionFiles(executionId, fromTimestamp, toTimestamp)
+        .forEach(supplier ->
+          addFrame(encoder, supplier));
+      executionSavingStateMap.put(executionId, SavingProgress.finished());
+      LOGGER.info("Finished saving video for execution {} (it took {}s)", executionId,
+        (System.currentTimeMillis() - startTime) / 1000L);
+    } catch (IOException | SavingException e) {
+      LOGGER.error("Error while saving video for execution {} (fps {})", e, fps, e);
+      executionSavingStateMap.put(executionId, SavingProgress.error());
+    }
+  }
+
+  private void addFrame(JpgSequenceEncoder encoder, IOSupplier<byte[]> bytesSupplier) {
+    try {
+      LOGGER.debug("Encoding frame {} (out of {})", framesProcessed.get(), framesCount);
+      byte[] bytes = bytesSupplier.get();
+      encoder.addFrame(bytes);
+      updateProgress();
+    } catch (IOException e) {
+      throw new SavingException("Error while adding frame", e);
+    }
+  }
+
+  private void updateProgress() {
+    int percentage = (int) (100L * framesProcessed.incrementAndGet() / framesCount);
+    executionSavingStateMap.put(executionId, SavingProgress.onGoing(percentage));
+  }
+}
