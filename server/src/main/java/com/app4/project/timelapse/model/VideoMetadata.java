6f07af1edebb2524a477d8961ec07e2900450415
nelson888 <tambue@hotmail.fr>
2020-03-27 00:05:58 +0100

    added mongo annotations

--- server/src/main/java/com/app4/project/timelapse/model/VideoMetadata.java
+++ server/src/main/java/com/app4/project/timelapse/model/VideoMetadata.java
@@ -1,10 +1,14 @@
 package com.app4.project.timelapse.model;
 
 import lombok.Value;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Value
+@Document
 public class VideoMetadata {
 
+  @Id
   int videoId;
   int executionId;
   int fps;
