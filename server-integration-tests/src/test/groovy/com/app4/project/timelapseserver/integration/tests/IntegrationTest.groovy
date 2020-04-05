33c6c8c7151ba51706acfd8c40ccb1b962f47a2f
nelson888 <tambue@hotmail.fr>
2020-04-05 16:42:50 +0200

    move custom client in separate class

--- server-integration-tests/src/test/groovy/com/app4/project/timelapseserver/integration/tests/IntegrationTest.groovy
+++ server-integration-tests/src/test/groovy/com/app4/project/timelapseserver/integration/tests/IntegrationTest.groovy
@@ -1,17 +1,9 @@
 package com.app4.project.timelapseserver.integration.tests
 
-import com.app4.project.timelapseserver.integration.tests.util.RestResponseException
-import groovyx.net.http.ContentType
-import groovyx.net.http.HTTPBuilder
-import groovyx.net.http.HttpResponseDecorator
-import groovyx.net.http.HttpResponseException
-import groovyx.net.http.RESTClient
-import org.apache.http.client.ClientProtocolException
+import com.app4.project.timelapseserver.integration.tests.util.CustomRestClient
 import spock.lang.Shared
 import spock.lang.Specification
 
-
-// super class with some cool methods
 abstract class IntegrationTest extends Specification {
 
     @Shared
@@ -21,45 +13,4 @@ abstract class IntegrationTest extends Specification {
         return System.currentTimeMillis()
     }
 
-    static class CustomRestClient extends RESTClient {
-        CustomRestClient(def baseUri) {
-            super(baseUri)
-        }
-
-        @Override
-        HttpResponseDecorator get(Map<String, ?> args) throws ClientProtocolException, IOException, URISyntaxException {
-            return super.get(args)
-        }
-
-        @Override
-        HttpResponseDecorator delete(Map<String, ?> args) throws URISyntaxException, ClientProtocolException, IOException {
-            return super.delete(args)
-        }
-
-        @Override
-        HttpResponseDecorator patch(Map<String, ?> args) throws URISyntaxException, ClientProtocolException, IOException {
-            return super.patch(args)
-        }
-
-        @Override
-        HttpResponseDecorator post(Map<String, ?> args) throws URISyntaxException, ClientProtocolException, IOException {
-            args.requestContentType = ContentType.JSON
-            return super.post(args)
-        }
-
-        @Override
-        HttpResponseDecorator put(Map<String, ?> args) throws URISyntaxException, ClientProtocolException, IOException {
-            args.requestContentType = ContentType.JSON
-            return super.put(args)
-        }
-
-        @Override
-        protected Object doRequest(HTTPBuilder.RequestConfigDelegate delegate) throws ClientProtocolException, IOException {
-            try {
-                return super.doRequest(delegate)
-            } catch (HttpResponseException e) {
-                throw new RestResponseException(e, e.response, e.response.data)
-            }
-        }
-    }
 }
---
+++ server-integration-tests/src/test/groovy/com/app4/project/timelapseserver/integration/tests/util/CustomRestClient.groovy
@@ -0,0 +1,49 @@
+package com.app4.project.timelapseserver.integration.tests.util
+
+import groovyx.net.http.HTTPBuilder
+import groovyx.net.http.HttpResponseDecorator
+import groovyx.net.http.HttpResponseException
+import groovyx.net.http.RESTClient
+import org.apache.http.client.ClientProtocolException
+
+class CustomRestClient extends RESTClient {
+        CustomRestClient(def baseUri) {
+            super(baseUri)
+        }
+
+        @Override
+        HttpResponseDecorator get(Map<String, ?> args) throws ClientProtocolException, IOException, URISyntaxException {
+            return super.get(args)
+        }
+
+        @Override
+        HttpResponseDecorator delete(Map<String, ?> args) throws URISyntaxException, ClientProtocolException, IOException {
+            return super.delete(args)
+        }
+
+        @Override
+        HttpResponseDecorator patch(Map<String, ?> args) throws URISyntaxException, ClientProtocolException, IOException {
+            return super.patch(args)
+        }
+
+        @Override
+        HttpResponseDecorator post(Map<String, ?> args) throws URISyntaxException, ClientProtocolException, IOException {
+            args.requestContentType = ContentType.JSON
+            return super.post(args)
+        }
+
+        @Override
+        HttpResponseDecorator put(Map<String, ?> args) throws URISyntaxException, ClientProtocolException, IOException {
+            args.requestContentType = ContentType.JSON
+            return super.put(args)
+        }
+
+        @Override
+        protected Object doRequest(HTTPBuilder.RequestConfigDelegate delegate) throws ClientProtocolException, IOException {
+            try {
+                return super.doRequest(delegate)
+            } catch (HttpResponseException e) {
+                throw new RestResponseException(e, e.response, e.response.data)
+            }
+        }
+    }
