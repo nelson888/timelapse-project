05029885a4e8c1430900bb3456f6e33fb4300fc4
nelson888 <tambue@hotmail.fr>
2020-04-05 13:52:47 +0200

    added id pool to generate ids

--- server/src/main/java/com/app4/project/timelapseserver/repository/fake/FakeExecutionRepository.java
+++ server/src/main/java/com/app4/project/timelapseserver/repository/fake/FakeExecutionRepository.java
@@ -4,6 +4,7 @@ import com.app4.project.timelapse.model.Execution;
 import com.app4.project.timelapseserver.exception.NotFoundException;
 import com.app4.project.timelapseserver.model.request.ExecutionPatchRequest;
 import com.app4.project.timelapseserver.repository.ExecutionRepository;
+import com.app4.project.timelapseserver.util.IdPool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
@@ -11,7 +12,6 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.BlockingQueue;
@@ -114,35 +114,4 @@ public class FakeExecutionRepository implements ExecutionRepository {
       ex.setTitle(request.getTitle());
     }
   }
-
-  private static class IdPool {
-
-    private final boolean[] took;
-
-    public IdPool(int max) {
-      took = new boolean[max];
-      Arrays.fill(took, false);
-    }
-
-
-    public int get() {
-      int i = 0;
-      while (took[i]) {
-        i++;
-        if (i >= took.length) {
-          throw new RuntimeException("There isn't any free id");
-        }
-      }
-      took[i] = true;
-      return i;
-    }
-
-    public void free(int i) {
-      if (i < 0 || i >= took.length) {
-        return;
-      }
-      took[i] = false;
-    }
-  }
-
 }
--- server/src/main/java/com/app4/project/timelapseserver/repository/mongo/MongoExecutionRepository.java
+++ server/src/main/java/com/app4/project/timelapseserver/repository/mongo/MongoExecutionRepository.java
@@ -3,6 +3,7 @@ package com.app4.project.timelapseserver.repository.mongo;
 import com.app4.project.timelapse.model.Execution;
 import com.app4.project.timelapseserver.model.request.ExecutionPatchRequest;
 import com.app4.project.timelapseserver.repository.ExecutionRepository;
+import com.app4.project.timelapseserver.util.IdPool;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -12,12 +13,15 @@ import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+import static com.app4.project.timelapseserver.configuration.ApplicationConfiguration.MAX_EXECUTIONS;
 
 @Profile("mongodb")
 @Component
 public class MongoExecutionRepository extends MongoRepository<Execution> implements ExecutionRepository {
 
   private static final String COLLECTION_NAME = "Execution";
+
+  private final IdPool idPool = new IdPool(MAX_EXECUTIONS);
 
   public MongoExecutionRepository(MongoTemplate mongoTemplate) {
     super(mongoTemplate, Execution.class, COLLECTION_NAME);
@@ -30,6 +34,21 @@ public class MongoExecutionRepository extends MongoRepository<Execution> impleme
     return Optional.ofNullable(
       mongoTemplate.findOne(query, Execution.class, COLLECTION_NAME)
     );
+  }
+
+  @Override
+  public void add(Execution object) {
+    object.setId(idPool.get());
+    super.add(object);
+  }
+
+  @Override
+  public boolean remove(int id) {
+    if (super.remove(id)) {
+      idPool.free(id);
+      return true;
+    }
+    return false;
   }
 
   @Override
