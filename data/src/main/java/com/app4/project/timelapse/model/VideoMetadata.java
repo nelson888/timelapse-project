ef6f12f0d137ea266ddb693592e131fab6dabbc2
nelson888 <tambue@hotmail.fr>
2020-04-07 12:26:30 +0200

    added document annotation to video metadata

--- data/src/main/java/com/app4/project/timelapse/model/VideoMetadata.java
+++ data/src/main/java/com/app4/project/timelapse/model/VideoMetadata.java
@@ -3,7 +3,9 @@ package com.app4.project.timelapse.model;
 import lombok.Data;
 import lombok.Value;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
 @Data
 @Value
 public class VideoMetadata {
